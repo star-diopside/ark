7dacae2bcfb3ec862fc2e43023042c366721204b
se-yoshi <se.yoshi@gmail.com>
2017-08-27 12:54:26 +0900

    ユーザ削除ジョブの構成を変更

--- spark-projects/spark-batch/src/main/java/jp/gr/java_conf/star_diopside/spark/batch/logic/UserVersionCheckProcessor.java
+++ spark-projects/spark-batch/src/main/java/jp/gr/java_conf/star_diopside/spark/batch/logic/UserVersionCheckProcessor.java
@@ -2,20 +2,19 @@ package jp.gr.java_conf.star_diopside.spark.batch.logic;
 
 import javax.inject.Inject;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.batch.item.ItemProcessor;
 
 import jp.gr.java_conf.star_diopside.spark.data.entity.User;
 import jp.gr.java_conf.star_diopside.spark.data.repository.UserRepository;
 
-public class UserVersionCheckProcessor implements ItemProcessor<Pair<String, Integer>, User> {
+public class UserVersionCheckProcessor implements ItemProcessor<User, User> {
 
     @Inject
     private UserRepository userRepository;
 
     @Override
-    public User process(Pair<String, Integer> item) throws Exception {
-        User user = userRepository.findOne(item.getLeft());
-        return user.getVersion() == item.getRight() ? user : null;
+    public User process(User item) throws Exception {
+        User user = userRepository.findOne(item.getUserId());
+        return user.getVersion() == item.getVersion() ? user : null;
     }
 }
