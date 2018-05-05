6ecca176f5db05268513481a9349e1bcb36c94a6
se-yoshi <se.yoshi@gmail.com>
2018-05-05 16:46:41 +0900

    ログ出力設定の変更

--- ci/scripts/install_repository.sh
+++ ci/scripts/install_repository.sh
@@ -2,6 +2,6 @@
 rm -rf ~/.gradle
 rm -rf ~/.m2
 cd $(dirname $(dirname $0))
-git clone -b v1.0.0 https://github.com/star-diopside/silver-commons.git
+git clone -b v1.0.1 https://github.com/star-diopside/silver-commons.git
 cd silver-commons
 ./gradlew clean install
